ceac86aa678be0194706e1b150f45681c70f9fc4
SebbeZivic <sebastianzsebbe@gmail.com>
2026-07-13 22:43:56 +0200

    Fix CORS: allow Netlify domain

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: /^http:\/\/localhost(:\d+)?$/,
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://swedmarksbil.netlify.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
